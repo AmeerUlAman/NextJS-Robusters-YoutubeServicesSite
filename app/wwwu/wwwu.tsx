d0b15f907b8df6c672e27256e226c41bbcedf2f2
Ameer Ul Aman Jodad Sultan <w.ameerulaman@gmail.com>
2024-08-05 02:04:56 +0500

    wwwu update

--- app/wwwu/wwwu.tsx
+++ app/wwwu/wwwu.tsx
@@ -6,7 +6,8 @@ const Wwwu = () => {
   return (
     <div style={{marginTop:"55px"}} id='wwwu'>   <h2 className={styles.h2} >Why Work With Us</h2>
     <div className={styles.container} >
-      <div>
+      <div className={styles.fi}>
+        <center><Image width={150} height={150} src={'/brandimage.png'} quality={100} alt='BRAND' className={styles.ch}/></center>
  
     <h3 className={styles.h3}>We Build Lasting Brands, Not Just Channels</h3>
     <p className={styles.p}>
@@ -15,13 +16,21 @@ const Wwwu = () => {
       fanbase that keeps coming back for more. With Robustors, you're not just
       another channel – you're a brand that viewers remember and engage with.
     </p>
-    <h3 className={styles.h3}>Multiple Income Streams</h3>
+    </div>
+    <div className={styles.fi}>
+      <center> <Image width={150} height={150} src={'/passiveincome.png'} quality={100} alt='multiple source income' className={styles.ch}/></center>
+   
+    <h3 className={styles.h3}>Multiple Income Streams, Not Just a Single One</h3>
     <p className={styles.p}>
       We don’t rely solely on AdSense. We focus on developing multiple revenue
       streams, including attracting sponsorships, setting up affiliate programs,
       creating merchandise, selling products, and optimizing channel memberships.
       We ensure your channel isn't just popular – it's profitable in multiple ways.
     </p>
+    </div>
+    <div className={styles.fi}>
+      <center><Image width={150} height={150} src={'/promotion.png'} quality={100} alt='promotion' className={styles.ch}/></center>
+    
     <h3 className={styles.h3}>Hands-Off Success, Long-Term Growth</h3>
     <p className={styles.p}>
       You bring the ideas, we handle everything else. From scripting to uploading,
@@ -31,9 +40,9 @@ const Wwwu = () => {
       YouTube team without the stress of day-to-day management.
     </p>
     </div>
-    <div style={{margin:"5% 0px 0px 10% "}} className={styles.im}>
+    {/* <div style={{margin:"5% 0px 0px 10% "}} className={styles.im}>
   <Image   width={329} height={300} quality={100} src={"/profic.png"} alt='PROFIT'/> 
-    </div>
+    </div> */}
   </div>
   </div>)
 }
