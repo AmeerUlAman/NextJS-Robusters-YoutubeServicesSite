b60028799673cf16f821116c1aac073e7a7c33c9
Ameer Ul Aman Jodad Sultan <w.ameerulaman@gmail.com>
2024-08-05 03:43:06 +0500

    responsiveness changes

--- app/vidtiers/vidtier.tsx
+++ app/vidtiers/vidtier.tsx
@@ -6,7 +6,7 @@ const Vidtier=()=>{
     <div id="vidtiers">
       <div className={styles.extmar}></div>
       <center><h1 className={styles.heading}>Tier Section</h1></center>
-     <center><p>Every YouTube channel has unique requirements. That's why we offer three tiers of
+     <center><p className={styles.pp}>Every YouTube channel has unique requirements. That's why we offer three tiers of
      video quality to suit your specific needs and budget.</p></center>
       <Tier/>
     </div>
